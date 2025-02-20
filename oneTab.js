e36a15012f9d36a26ed314db68dd985f061e3685
Wegt-meoh <wegt-meog@outlook.com>
2025-02-20 22:12:40 +0800

    doc: remove safari part feat: change volume change step

--- oneTab.js
+++ oneTab.js
@@ -6,3 +6,37 @@ document.addEventListener('click', (e) => {
         window.location.href = link.href;
     }
 });
+
+const video = document.querySelector("video");
+video.focus();
+
+document.addEventListener('keydown', (e) => {
+    const volumeHint = document.querySelector(".bpx-player-volume-hint");
+    const volumeHintText = volumeHint.querySelector(".bpx-player-volume-hint-text");
+
+    if (!video || !volumeHint || !volumeHintText) {
+        return;
+    }
+
+    if (e.key !== "ArrowUp" && e.key !== "ArrowDown") {
+        return;
+    }
+
+    video.muted = false;
+    e.preventDefault();
+
+    const flag = e.key === "ArrowDown" ? false : true;
+
+    if (video === document.activeElement) {
+        video.volume = (Math.round(video.volume * 100) - (flag ? 5 : -5)) / 100;
+        return;
+    }
+
+    video.volume = (Math.round(video.volume * 100) + (flag ? 5 : -5)) / 100;
+    volumeHintText.innerText = `${Math.round(video.volume * 100)}%`;
+
+    volumeHint.style = "opacity: 1;";
+    setTimeout(() => {
+        volumeHint.style = "opacity: 0;display:none;";
+    }, 1000);
+});
