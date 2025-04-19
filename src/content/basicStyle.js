36bde07a22e4ac10c2d12752aeb1cc9deb29580c
wegt-meoh <wegt-meoh@outlook.com>
2025-04-20 02:15:49 +0800

    fix: add class for basic style element

--- src/content/basicStyle.js
+++ src/content/basicStyle.js
@@ -1,7 +1,8 @@
-import { BASIC_STYLE } from "./const";
+import { BASIC_STYLE, CLASS_PREFIX } from "./const";
 
 export function injectBasicStyle() {
     const styleEle = document.createElement('style');
+    styleEle.classList.add(`${CLASS_PREFIX}-basic`);
     styleEle.textContent = BASIC_STYLE;
     (document.head || document.documentElement).appendChild(styleEle);
 }
