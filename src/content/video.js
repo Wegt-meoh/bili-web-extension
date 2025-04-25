9dfc2a871231aac58a06226ed7989b3b75ba0d22
wegt-meoh <wegt-meoh@outlook.com>
2025-04-25 15:21:03 +0800

    fix: ensure video is focus when arrow key press

--- src/content/video.js
+++ src/content/video.js
@@ -1,28 +1,22 @@
 export function modifyVideo() {
-    const video = document.querySelector("video");
-    if (video) {
-        video.tabIndex = -1;
-
-        document.addEventListener('keydown', (e) => {
-            const volumeHint = document.querySelector(".bpx-player-volume-hint");
-            const volumeHintText = volumeHint?.querySelector(".bpx-player-volume-hint-text");
-
-            if (!video || !volumeHint || !volumeHintText) {
-                return;
-            }
-
-            if (e.key !== "ArrowUp" && e.key !== "ArrowDown") {
-                return;
-            }
-
-            video.muted = false;
-
-            const flag = e.key === "ArrowDown" ? false : true;
-
-            video.focus();
-            const val = (Math.round(video.volume * 100) - (flag ? 5 : -5)) / 100;
-            video.volume = val < 0 ? 0 : val > 1 ? 1 : val;
+    let video;
+    document.addEventListener('keydown', (e) => {
+        if (e.key !== "ArrowUp" && e.key !== "ArrowDown") {
             return;
-        });
-    }
+        }
+        if (!video) {
+            video = document.querySelector("video");
+            video.tabIndex = -1;
+        }
+
+        if (!video) {
+            return;
+        }
+
+        video.muted = false;
+        const flag = e.key === "ArrowDown" ? false : true;
+        video.focus();
+        const val = (Math.round(video.volume * 100) - (flag ? 5 : -5)) / 100;
+        video.volume = val < 0 ? 0 : val > 1 ? 1 : val;
+    });
 }
