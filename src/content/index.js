a8cd203d61426757d16b93c3cb00a5a35cd60074
wegt-meoh <wegt-meoh@outlook.com>
2025-04-25 14:12:21 +0800

    fix: message background image

--- src/content/index.js
+++ src/content/index.js
@@ -8,6 +8,13 @@ injectBasicStyle();
 document.addEventListener("DOMContentLoaded", () => {
     // ensure the sync cssom onload
     setupDomListener(document.documentElement);
+
+    const messageBgElme = document.querySelector(".message-bg");
+    if (messageBgElme) {
+        const style = messageBgElme.getAttribute("style");
+        messageBgElme.setAttribute("style", style.replace("light", "dark"));
+    }
+
     modifyVideo();
     modifyNavigation();
 });
